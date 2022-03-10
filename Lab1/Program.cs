c9e708c4bc382f8ea33aede4ebf570c50446ae9d
tomasz.ciuk <tomasz.ciuk@PK2-10>
2022-03-10 12:12:47 +0100

    Dodaj pliki projektów.

---
+++ Lab1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Ulamek ulamek = new Ulamek();
+            Ulamek ulamek2 = new Ulamek();
+            Ulamek ulamek3 = ulamek + ulamek2;
+        }
+    }
+    class Ulamek
+    {
+        uint licznik, mianownik;
+        public Ulamek()
+        {
+
+        }
+        public Ulamek(uint licznik, uint mianownik)
+        {
+            this.licznik = licznik;
+            this.mianownik = mianownik;
+
+        }
+        public Ulamek(Ulamek ulamek)
+        {
+            this.licznik = ulamek.licznik;
+        }
+        public override string ToString()
+        {
+            return "Tekstowa reprezentacja";
+        }
+        public static Ulamek operator +(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.mianownik + b.licznik * a.mianownik, a.mianownik * b.mianownik);
+        }
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.mianownik - b.licznik * a.mianownik, a.mianownik * b.mianownik);
+        }
+        public static Ulamek operator *(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.licznik, a.mianownik * b.mianownik);
+        }
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+        }
+
+
+    }
+}
