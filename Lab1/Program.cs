030c17adb67f943a8920e776a91fa52e1290f393
Tomasz Ciuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-03-11 10:29:41 +0100

    Dodano interfajs IComparable

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,13 +6,29 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Ulamek ulamek = new Ulamek();
-            Ulamek ulamek2 = new Ulamek();
+            Ulamek ulamek = new Ulamek(2,3);
+            Ulamek ulamek2 = new Ulamek(3,2);
             Ulamek ulamek3 = ulamek + ulamek2;
-            Console.WriteLine("Test Gita");
+            Console.WriteLine(ulamek3.ToString());
+
+            Ulamek[] ulamki = new Ulamek[]
+        {
+            new Ulamek(4, 2),
+            new Ulamek(3, 6),
+            new Ulamek(4, 10)
+        };
+            Console.WriteLine("Nie posortowane: ");
+            for (int i = 0; i < ulamki.Length; ++i)
+                Console.WriteLine(ulamki[i]);
+
+            Array.Sort(ulamki); // podczas sortowania wykorzystana zostanie metoda CompareTo()
+
+            Console.WriteLine("Posortowane: ");
+            for (int i = 0; i < ulamki.Length; ++i)
+                Console.WriteLine(ulamki[i]);
         }
     }
-    class Ulamek
+    public class Ulamek: IComparable<Ulamek>
     {
         uint licznik, mianownik;
         public Ulamek()
@@ -31,8 +47,21 @@ namespace Lab1
         }
         public override string ToString()
         {
-            return "Tekstowa reprezentacja";
+            return licznik+"/"+mianownik;
         }
+
+        public int CompareTo(Ulamek other)
+        {
+            if (other == null) return -1;
+            if (other == this) return 0;
+
+            if (this.mianownik > other.mianownik && this.licznik > other.licznik) return +1;
+            if (this.mianownik < other.mianownik && this.licznik < other.licznik) return -1;
+
+            return 0;
+
+        }
+
         public static Ulamek operator +(Ulamek a, Ulamek b)
         {
             return new Ulamek(a.licznik * b.mianownik + b.licznik * a.mianownik, a.mianownik * b.mianownik);
