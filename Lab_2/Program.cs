7032d33f77e61568fa245cb1aa55c8b6acaab512
Tomasz Ciuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-03-18 11:31:08 +0100

    Tworzenie klasy student, task, enum

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_2
 {
     public class Person
     {
-        String name { get; }
-        int age { get; }
-        public Person (String name, int age)
+        protected string name { get; set; }
+        protected int age { get; set; }
+        public Person (string name, int age)
         {
             this.name = name;
             this.age = age;
@@ -21,6 +22,64 @@ namespace Lab_2
         public override string ToString()
         {
             return name + "(" + age + " y.o)";
+        }
+    }
+    public enum TaskStatus
+    {
+        Waiting,
+        Progress,
+        Done,
+        Rejected
+    }
+    public class Task
+    {
+        private String name { get; set; }
+        private TaskStatus status { get; set; }
+        public Task (string name, TaskStatus status)
+        {
+            this.name = name;
+            this.status = status;
+        }
+        public bool Equals(Task other)
+        {
+            if (other == null) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.name, other.name) && Object.Equals(this.status, other.status);
+        }
+        public override string ToString()
+        {
+            return "Task "+name+" ["+status+"]";
+        }
+    }
+    public class Student : Person
+    {
+        protected string group { get; set; }
+        protected List<Task> tasks = new List<Task>();
+        public Student (string name, int age, string group) : base (name,age)
+        {
+            this.name = name;
+            this.age = age;
+            this.group = group;
+        }
+        public Student(string name, int age, string group, List<Task> tasks) : base(name, age)
+        {
+            this.name = name;
+            this.age = age;
+            this.group = group;
+            this.tasks = tasks;
+        }
+        public void AddTask (string taskName, TaskStatus taskStatus)
+        {
+            tasks.Add(new Task(taskName, taskStatus);
+        }
+        public void RemoveTask (int index)
+        {
+            tasks.RemoveAt(index);
+        }
+        public void UpdateTask(int index, TaskStatus taskStatus)
+        {
+            tasks[index] = taskStatus; 
         }
     }
     class Program
