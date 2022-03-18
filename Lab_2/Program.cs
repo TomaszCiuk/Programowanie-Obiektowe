0bdeb9164784c5a1962c4316eb80595e1674ac6c
Tomasz Ciuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-03-18 10:25:36 +0100

    Dodanie klasy Person

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -2,6 +2,27 @@
 
 namespace Lab_2
 {
+    public class Person
+    {
+        String name { get; }
+        int age { get; }
+        public Person (String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public bool Equals(Person other)
+        {
+            if (other == null) return false;
+            if (other == this) return true;
+
+            return Object.Equals(this.name, other.name) && Object.Equals(this.age, other.age);
+        }
+        public override string ToString()
+        {
+            return name + "(" + age + " y.o)";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
