7a80d26f3bd7ce026d3c26a3a010161118157bce
Tomasz Ciuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-03-25 11:31:25 +0100

    Dodano Lab 3

---
+++ Lab_3/Logger/CommonLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3.Logger
+{
+    class CommonLogger : ILogger
+    {
+        ILogger[] loggers;
+
+        public CommonLogger (ILogger loggers)
+        {
+            this.loggers = loggers;
+        }
+        public void Dispose()
+        {
+        }
+
+        public void Log(params string[] messages)
+        {
+            string time = DateTime.Now.ToString("yyyy - MM - ddTHH:mm: ssZ");
+
+            for (int i = 0; i < messages.Length; i++)
+            {
+
+                this.loggers[i] = (time + ": " + messages[i] + "\n");
+            }
+        }
+    }
+}
