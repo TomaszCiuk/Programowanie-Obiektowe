a90339a357455975928fc65f4d4700d0da26f321
Tomasz Ciuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-04-28 09:46:57 +0200

    Dodano Lab 5

---
+++ Lab_5/Observable/ObservableList2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5.Observable
+{
+    class ObservableList2<T>
+    {
+        private List<T> list = new List<T>();
+        private int lenght;
+        public int Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                lenght = value;
+            }
+        }
+
+        public event EventHandler<AddedEventArgs<T>> Added;
+        protected virtual void OnAdded(AddedEventArgs<T> e)
+        {
+            EventHandler<AddedEventArgs<T>> handler = this.Added;
+
+            handler?.Invoke(this, e);
+        }
+
+        public event EventHandler<AddedEventArgs<T>> Setted;
+        protected virtual void OnSetted(AddedEventArgs<T> e)
+        {
+            EventHandler<AddedEventArgs<T>> handler = this.Setted;
+
+            handler?.Invoke(this, e);
+        }
+
+        public event EventHandler<AddedEventArgs<T>> Removed;
+        protected virtual void OnRemoved(AddedEventArgs<T> e)
+        {
+            EventHandler<AddedEventArgs<T>> handler = this.Removed;
+
+            handler?.Invoke(this, e);
+        }
+
+        internal void Add(T value)
+        {
+            this.list.Add(value);
+
+            this.OnAdded(new AddedEventArgs<T> { Index = this.list.Count - 1, Value = value });
+        }
+        internal void Set(int index, T value)
+        {
+            this.list[index] = value;
+
+            this.OnSetted(new AddedEventArgs<T> { Index = index, Value = value });
+        }
+        internal T Get(int index)
+        {
+            return this.list[index];
+        }
+        internal void RemoveAt(int index)
+        {
+            this.list.RemoveAt(index);
+
+            this.OnRemoved(new AddedEventArgs<T> { Index = index });
+        }
+        public class AddedEventArgs<T> : EventArgs
+        {
+            public int Index { get; set; }
+            public T Value { get; set; }
+        }
+    }
+}
---
+++ Lab_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Lab_5.Observable;
+
+namespace Lab_5
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ObservableList1<int> list1 = new ObservableList1<int>();
+            list1.Add(1);
+            list1.Add(2);
+            list1.Add(3);
+            list1.Set(0, 5);
+            list1.Set(1, 5);
+            list1.Set(2, 5);
+            Console.WriteLine(list1.Get(2));
+            list1.RemoveAt(2);
+            list1.Add(10);
+            Console.WriteLine(list1.Get(2));
+        }
+    }
+}
