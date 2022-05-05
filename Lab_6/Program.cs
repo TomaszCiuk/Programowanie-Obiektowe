7c213f6a493fafc44c337a82ab7d09b731123cb4
TomaszCiuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-05-05 23:16:58 +0200

    Prawie zrobione zadanie 6

--- Lab_6/Program.cs
+++ Lab_6/Program.cs
@@ -10,26 +10,26 @@ namespace Lab_6
         {
             List<User> users = new List<User>()
             {
-                new User { Name = "Karol" , Role = "Student", Age = 23, Marks = new int[] {3, 3, 3, 4, 5 }},
-                new User { Name = "Maciek" , Role = "Student", Age = 21, Marks = new int[] {3, 3, 4, 4, 5, 4}},
-                new User { Name = "Bartek" , Role = "Student", Age = 22, Marks = new int[] {3, 4, 3, 4, 5, 3}},
-                new User { Name = "Wincenty" , Role = "Student", Age = 19, Marks = new int[] {4, 3, 4, 5, 4}},
-                new User { Name = "Robert" , Role = "Student", Age = 18, Marks = new int[] {3, 3, 3, 3, 3 }},
-                new User { Name = "Hubert" , Role = "Student", Age = 25, Marks = new int[] {4, 4, 4, 5, 5}},
-                new User { Name = "Piotrek" , Role = "Student", Age = 32, Marks = new int[] {4, 3, 4, 5, 5}},
-                new User { Name = "Bartek" , Role = "Student", Age = 35, Marks = new int[] {3, 3, 3, 4, 5}},
-                new User { Name = "Daniel" , Role = "Student", Age = 33, Marks = new int[] {4, 3, 4, 4, 5,}},
-                new User { Name = "Dimitro" , Role = "Student", Age = 26, Marks = new int[] {3, 4, 4, 4, 5} },
+                new User { Name = "Karol" , Role = "Student", Age = 23, Marks = new int[] {3, 3, 3, 4, 5 }, CreatedAt = new DateTime(2017, 1, 2)},
+                new User { Name = "Maciek" , Role = "Student", Age = 21, Marks = new int[] {3, 3, 4, 4, 5, 4}, CreatedAt = new DateTime(2017, 3, 4)},
+                new User { Name = "Bartek" , Role = "Student", Age = 22, Marks = new int[] {3, 4, 5, 3}, CreatedAt = new DateTime(2017, 2, 16)},
+                new User { Name = "Wincenty" , Role = "Student", Age = 19, Marks = new int[] {4, 5, 4}, CreatedAt = new DateTime(2017, 2, 16)},
+                new User { Name = "Robert" , Role = "Student", Age = 18, Marks = new int[] {3, 3, 3, 3, 3 }, CreatedAt = new DateTime(2017, 6, 16)},
+                new User { Name = "Hubert" , Role = "Student", Age = 25, Marks = new int[] {4, 4, 4, 5, 5}, CreatedAt = new DateTime(2017, 1, 2)},
+                new User { Name = "Piotrek" , Role = "Student", Age = 32, Marks = new int[] {4, 5, 5}, CreatedAt = new DateTime(2016, 3, 16), RemovedAt = new DateTime(2019, 3, 1)},
+                new User { Name = "Bartek" , Role = "Student", Age = 35, Marks = new int[] {3, 3, 3, 4, 5}, CreatedAt = new DateTime(2015, 3, 16), RemovedAt = new DateTime(2018, 2, 1)},
+                new User { Name = "Daniel" , Role = "Student", Age = 33, Marks = new int[] {4, 3, 4,}, CreatedAt = new DateTime(2017, 12, 3), RemovedAt = new DateTime(2019, 3, 1)},
+                new User { Name = "Dimitro" , Role = "Student", Age = 26, Marks = new int[] {3, 4}, CreatedAt = new DateTime(2014, 3, 16), RemovedAt = new DateTime(2017, 3, 1) },
                 new User { Name = "Damian" , Role = "Student", Age = 29, Marks = new int[] {} },
-                new User { Name = "Bartosz", Role = "Teacher", Age = 40, Marks = null },
-                new User { Name = "Tomek", Role = "Teacher", Age = 34, Marks = null  },
-                new User { Name = "Jacenty", Role = "Teacher", Age = 64, Marks = null  },
-                new User { Name = "Grzegorz", Role = "Teacher", Age = 47, Marks = null  },
-                new User { Name = "Karol", Role = "Teacher", Age = 39, Marks = null  },
-                new User { Name = "Wiktor", Role = "Moderator", Age = 34, Marks = null  },
-                new User { Name = "Norbert", Role = "Moderator", Age = 54, Marks = null  },
-                new User { Name = "Ulbert", Role = "Moderator", Age = 52, Marks = null   },
-                new User { Name = "Wit", Role = "Admin", Age = 65, Marks = null }
+                new User { Name = "Bartosz", Role = "Teacher", Age = 40, Marks = null, CreatedAt = new DateTime(2014, 3, 16)},
+                new User { Name = "Tomek", Role = "Teacher", Age = 34, Marks = null, CreatedAt = new DateTime(2016, 3, 16), RemovedAt = new DateTime(2019, 3, 1)  },
+                new User { Name = "Jacenty", Role = "Teacher", Age = 64, Marks = null, CreatedAt = new DateTime(2010, 3, 16)},
+                new User { Name = "Grzegorz", Role = "Teacher", Age = 47, Marks = null, CreatedAt = new DateTime(2008, 3, 16), RemovedAt = new DateTime(2018, 5, 2)  },
+                new User { Name = "Karol", Role = "Teacher", Age = 39, Marks = null, CreatedAt = new DateTime(2008, 3, 16) },
+                new User { Name = "Wiktor", Role = "Moderator", Age = 34, Marks = null, CreatedAt = new DateTime(2004, 1, 1)  },
+                new User { Name = "Norbert", Role = "Moderator", Age = 54, Marks = null, CreatedAt = new DateTime(2007, 7, 13)  },
+                new User { Name = "Ulbert", Role = "Moderator", Age = 52, Marks = null, CreatedAt = new DateTime(2008, 3, 16)   },
+                new User { Name = "Wit", Role = "Admin", Age = 65, Marks = null, CreatedAt = new DateTime(2004, 3, 16), RemovedAt = new DateTime(2010, 12, 31) }
             };
             //1. Liczba rekordów w tablicy
             //Console.WriteLine(users.Count);
@@ -64,7 +64,102 @@ namespace Lab_6
             //}
 
             //6. Ilość rekordów, dla których podano oceny (nie null i więcej niż 0)
-            Console.Write(users.Where(user => user.Marks is not null || user.Marks.Length > 0).Count());
+            var marks = users.Where(user => user.Marks != null && user.Marks.Length > 0).Select(user => user).Count();
+            //Console.WriteLine("Liczba rekordów dla których nie podano ocney: " + marks);
+            //Console.WriteLine("Liczba rekordów dla których nie podano ocney: " + (from user in users where user.Marks != null && user.Marks.Length > 0 select user).Count());
+
+            //7. --------- Suma,ilość i średnia wszystkich ocen studentow ---------
+            var countUsers_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 select user.Marks.Count()).Sum();
+            var sumUsers_1 = (from user in users where user.Marks != null && user.Marks.Length > 0  select user.Marks.Sum()).Sum();
+            var avgUsers_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 select user.Marks.Average()).Average();
+            var countUsers_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).Select(user => user.Marks.Count()).Sum();
+            var sumUsers_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).Select(user => user.Marks.Sum()).Sum();
+            var avgUsers_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).Select(user => user.Marks.Average()).Average();
+            //Console.WriteLine("Ilosc Ocen: " + countUsers_2 + "\n " + "Suma Ocen: " + sumUsers_2 + "\n " + "Srednia Ocen: " + avgUsers_2);
+
+            //8. --------- Najlepsza ocena ---------
+            var bestMark_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Max() descending select user.Marks.Max());
+            var bestMark_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderByDescending(user => user.Marks.Max()).Select(user => user.Marks.Max());
+            //foreach (var mark in bestMark_2.Take(1))
+            //{
+            //    Console.WriteLine("Najlepsza ocena to: " + mark);
+            //}
+
+            //9. --------- Najgorsza ocena ---------
+            var worstMark_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Min() ascending select user.Marks.Min());
+            var worstMark_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderBy(user => user.Marks.Min()).Select(user => user.Marks.Min());
+            //foreach (var mark in worstMark_2.Take(1))
+            //{
+            //    Console.WriteLine("Najgorsza ocena to: " + mark);
+            //}
+
+            //10. --------- Najlepszy Student ---------
+            var bestStudent_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Average() descending select user.Name);
+            var bestStudent_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderByDescending(user => user.Marks.Average()).Select(user => user.Name);
+            //foreach (var student in bestStudent_2.Take(1))
+            //{
+            //    Console.WriteLine("Najlepszym studentem jest: " + student);
+            //}
+
+            //11. --------- Liste studentów którzy posiadają najmniej ocen ---------
+            var leastMarks_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Length ascending select user).ToList();
+            var leastMarks_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderBy(user => user.Marks.Length).Select(user => user);
+            //Console.WriteLine("Najmniej ocen posiadają:");
+            //foreach (var user in leastMarks_2)
+            //{
+            //    Console.WriteLine(user.Name);
+            //}
+
+            //12. --------- Liste studentów którzy posiadają najwięcej ocen ---------
+            var mostMarks_1 = (from user in users  where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Length descending select user).ToList();
+            var mostMarks_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderByDescending(user => user.Marks.Length).Select(user => user);
+            //Console.WriteLine("Najwięcej ocen posiadają:");
+            //foreach (var user in mostMarks_2)
+            //{
+            //    Console.WriteLine(user.Name);
+            //}
+
+            //13. --------- Liste obiektów zawierających tylko nazwę i średnią ocenę(mapowanie na inny obiekt) ---------
+
+
+
+            //14. --------- Studentów posortowanych od najlepszego ---------
+            var bestStudentSortetList_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 orderby user.Marks.Average() descending select user.Name);
+            var bestStudentSortetList_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).OrderByDescending(user => user.Marks.Average()).Select(user => user.Name);
+            //foreach (var user in bestStudentSortetList_2)
+            //    Console.WriteLine(user);
+
+            //15. --------- Średnią ocenę wszystkich studentów ---------
+            var avgAllStudentMarks_1 = (from user in users where user.Marks != null && user.Marks.Length > 0 select user.Marks.Average()).Average();
+            var avgAllStudentMarks_2 = users.Where(user => user.Marks != null && user.Marks.Length > 0).Select(user => user.Marks.Average()).Average();
+            //Console.WriteLine("Średnia ocena wszystkich studentów to: " + avgAllStudentMarks_2);
+
+            //16. --------- Lista użytkowników pogrupowanych po miesiącach daty utworzenia (np. 2022-02,2022-03,2022-04, itp.) ---------
+            //var userCreateDateSortedList_1 = (from User user in users orderby user.CreatedAt group user by user.CreatedAt.Month);
+            //var userCreateDateSortedList_2 = users.GroupBy(user => user.CreatedAt.Month).OrderByDescending(user => -user.Key);
+            //foreach (var group in userCreateDateSortedList_1)
+            //{
+            //    Console.WriteLine(group.Key.ToString());
+            //    foreach (var user in group)
+            //    {
+            //        Console.WriteLine(user.Name);
+            //    }
+            //}
+
+            //17. --------- Lista użytkowników którzy nie zostali usunięci(data usunięcia nie została ustawiona) ---------
+            var usersNoRemoved_1 = (from user in users where user.RemovedAt == null select user).ToList();
+            var usersNoRemoved_2 = users.Where(user => user.RemovedAt == null).Select(user => user).ToList();
+            //foreach (var user in usersNoRemoved_1)
+            //    Console.WriteLine(user.Name);
+
+            //18. --------- Najnowszego Studenta(najnowsza data uwtorzenia) ---------
+            Console.WriteLine();
+            var latestStudent_1 = (from user in users orderby user.CreatedAt descending select user).ToList();
+            var latestStudent_2 = users.OrderByDescending(user => user.CreatedAt).Select(user => user).ToList();
+            //foreach (var user in latestStudent_2.Take(1))
+            //{
+            //    Console.WriteLine("Najnowszy student to: " + user.Name);
+            //}
         }
     }
     public class User
